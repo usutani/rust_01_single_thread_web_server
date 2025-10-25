01b8a1bf8ab667662c0aa5df58632b724696df36
Yasuhiro Usutani <y_usutani@mac.com>
2025-10-25 15:41:38 +0900

    Graceful Shutdown and Cleanup
    https://doc.rust-lang.org/book/ch21-03-graceful-shutdown-and-cleanup.html
    https://github.com/rust-lang/book/blob/main/listings/ch21-web-server/no-listing-07-final-code/hello.html

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,45 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::thread;
-use std::time::Duration;
+use hello::ThreadPool;
+use std::{
+    fs,
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else if buffer.starts_with(sleep) {
-        for i in 1..6 {
-            println!("Count: {}", i);
-            thread::sleep(Duration::from_secs(1));
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
         }
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
-    let mut file = File::open(filename).unwrap();
-    let mut contents = String::new();
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-    file.read_to_string(&mut contents).unwrap();
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    let response = format!("{}{}", status_line, contents);
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
 }
